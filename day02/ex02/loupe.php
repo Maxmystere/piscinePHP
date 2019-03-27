a0da3487d29e8aff5040c4ab938e3b18fbb8bfb4
Maxence GRAB <magrab@e3r10p18.42.fr>
2019-03-27 17:19:42 +0100

    Ex02 completed

--- day02/ex02/loupe.php
+++ day02/ex02/loupe.php
@@ -3,9 +3,36 @@
 if ($argc == 2)
 {
 	$html = file_get_contents($argv[1]);
-	$exp = explode('title', $html);
-	if (preg_match("/\s{0,}=/", $exp[3]) > 0)
-		print($exp[3]);
-	//print_r($exp);
+	$exp = explode("title", $html);
+	foreach($exp as $exppart)
+	{
+		if (preg_match("/(<a|=\")(.*?)>(.*?)</", $exppart) > 0)
+		{
+			unset($nchild);
+			$child = explode("\"", $exppart);
+			foreach($child as $lilchild)
+			{
+				if (!preg_match("/[<>]/", $lilchild))
+					$lilchild = strtoupper($lilchild);
+				$nchild[] = $lilchild;
+			}
+			$exppart = implode("\"", $nchild);
+			$x = 0;
+			$in = false;
+			while ($exppart[$x])
+			{
+				if ($exppart[$x] == '>')
+					$in = true;
+				else if ($exppart[$x] == '<')
+					$in = false;
+				else if ($in)
+					$exppart[$x] = strtoupper($exppart[$x]);
+				$x++;
+			}
+		}
+		$newexp[] = $exppart;
+	}
+	$perfect = implode("title", $newexp);
+	print($perfect);
 }
 ?>
