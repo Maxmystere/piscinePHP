f7775a14c617443258aab6efed7d50c528b7e6d5
Maxence GRAB <magrab@e3r10p18.42.fr>
2019-03-28 00:54:51 +0100

    Done

---
+++ day02/ex04/photos.php
@@ -0,0 +1,31 @@
+#!/usr/bin/php
+<?PHP
+if ($argc == 2)
+{
+	$c = curl_init($argv[1]);
+
+	curl_setopt($c, CURLOPT_RETURNTRANSFER, true);
+	$str = curl_exec($c);
+	$tab = explode("\n", $str);
+	$tab = preg_grep("/<img.*?>/", $tab);
+	if ($tab)
+	{
+		$path = explode("/", $argv[1]);
+		$tab = array_map("trim", $tab);
+		//print_r($tab);
+		if (!file_exists($path[2]))
+			mkdir($path[2]);
+		foreach($tab as $imgloc)
+		{
+			$imgloc = substr($imgloc, strpos($imgloc, "src="));
+			$tmp = explode("\"", $imgloc);
+			if (count($tmp) == 1)
+				$tmp = explode("'", $imgloc);
+			$imgloc = $tmp[1];
+			if (!preg_match("/https{0,1}:\/\//", $imgloc))
+				$imgloc = $path[0]."//".$path[2].$imgloc;
+			file_put_contents($path[2]."/".(array_pop(explode("/", $imgloc))), file_get_contents($imgloc));
+		}
+	}
+}
+?>
