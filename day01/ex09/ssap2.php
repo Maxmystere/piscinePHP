06dc6f8d5eb41ece1c7af03279a0f36c80e5bbe8
Maxence GRAB <magrab@e3r10p18.42.fr>
2019-03-27 15:43:52 +0100

    Correc day01 ex09

--- day01/ex09/ssap2.php
+++ day01/ex09/ssap2.php
@@ -4,9 +4,9 @@ array_shift($argv);
 $merge = array();
 foreach ($argv as $av)
 {
-    $tab = array_filter(explode(" ", trim($av)));
-    $tab = array_map("trim", $tab);
-    $merge = array_merge($merge, $tab);
+	$tab = array_filter(explode(" ", trim($av)));
+	$tab = array_map("trim", $tab);
+	$merge = array_merge($merge, $tab);
 	natcasesort($merge);
 }
 $issort = true;
@@ -17,14 +17,16 @@ while ($issort)
 	while ($merge[$x] && $merge[$x + 1])
 	{
 		$merge = array_values($merge);
-		if (((ctype_digit($merge[$x])) && ctype_alpha($merge[$x + 1])) || (!ctype_alnum($merge[$x]) && ctype_alnum($merge[$x + 1])))
+		if (((ctype_digit($merge[$x])) && ctype_alpha($merge[$x + 1]))
+			|| (!ctype_alnum($merge[$x]) && ctype_alnum($merge[$x + 1])))
 		{
 			$tmp = $merge[$x];
 			$merge[$x] = $merge[$x + 1];
 			$merge[$x + 1] = $tmp;
 			$issort = true;
 		}
-		if (ctype_digit($merge[$x]) && ctype_digit($merge[$x + 1]) && strcmp($merge[$x], $merge[$x + 1]) > 0)
+		if (ctype_digit($merge[$x]) && ctype_digit($merge[$x + 1])
+			&& strcmp($merge[$x], $merge[$x + 1]) > 0)
 		{
 			$tmp = $merge[$x];
 			$merge[$x] = $merge[$x + 1];
@@ -37,13 +39,6 @@ while ($issort)
 }
 foreach ($merge as $my)
 {
-	print("$my\tdigit : ");
-	print(ctype_digit($my));
-	print("\talpha : ");
-	print(ctype_alpha($my));
-	print("\talnum : ");
-	print(ctype_alnum($my));
-	print("\n");
+	print("$my\n");
 }
-print_r($merge);
 ?>
